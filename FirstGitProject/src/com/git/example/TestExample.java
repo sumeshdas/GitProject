f84e5c95fafe222831c787631613d72e07a78472
sumesh <sumesh@192.168.5.9>
2019-06-07 20:35:02 +0530

    change some lines

--- FirstGitProject/src/com/git/example/TestExample.java
+++ FirstGitProject/src/com/git/example/TestExample.java
@@ -4,6 +4,13 @@ public class TestExample {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		System.out.println("Hello Bangaluru");
+		
+		System.out.println("Hello Odisha");
+		
+		System.out.println("Hello india");
+		
 
 	}
 
